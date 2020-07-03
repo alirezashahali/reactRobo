f9ef9001fc3a242bcd3f46d3a029c66f29ff573e
Alireza <shahali.alireza.92@gmail.com>
2020-07-03 11:59:50 +0430

    Updates

---
+++ precache-manifest.2a43f06830407e8ab8e4fbb1e5ea96c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30025952be1394d135e96778e603721f",
+    "url": "/reactRobo/index.html"
+  },
+  {
+    "revision": "94fc3fef1e5bc69f8ae6",
+    "url": "/reactRobo/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5e142b5ae3541e1c46a3",
+    "url": "/reactRobo/static/css/main.0bc84294.chunk.css"
+  },
+  {
+    "revision": "94fc3fef1e5bc69f8ae6",
+    "url": "/reactRobo/static/js/2.41e980b8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactRobo/static/js/2.41e980b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e142b5ae3541e1c46a3",
+    "url": "/reactRobo/static/js/main.682d527c.chunk.js"
+  },
+  {
+    "revision": "71ed49e136499ca961fe",
+    "url": "/reactRobo/static/js/runtime-main.604cdf07.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/reactRobo/static/media/SEGA.12a3db8b.woff"
+  }
+]);
